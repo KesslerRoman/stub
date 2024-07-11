23c77614707f187700561b9778c2841205506144
KesslerRoman <kesslerr@mail.ru>
2024-07-11 16:22:25 +0700

    Add operation with files

--- src/main/java/com/example/stub/controller/MethodsController.java
+++ src/main/java/com/example/stub/controller/MethodsController.java
@@ -6,7 +6,12 @@ import db.connection;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Random;
 
 
 @RestController
@@ -29,9 +34,30 @@ public class MethodsController {
     public ResponseEntity<?> get(@RequestParam String condition) {
         try {
             User user = connection.select(condition);
+            try(FileWriter writer = new FileWriter("files/output.csv", true)){
+                writer.write("\n" + user.getLogin() + "," + user.getEmail() + "," + user.getPassword() + "," + user.getDate());
+            }catch (IOException e){
+                System.out.println("No such file");
+            }
             return ResponseEntity.status(200).body(user);
         }catch (SQLException e) {
             return ResponseEntity.status(500).body(e.getMessage());
+        }
+    }
+
+
+    @GetMapping("/random")
+    public ResponseEntity<?> random() {
+        try(BufferedReader reader = new BufferedReader(new FileReader("files/random.txt"))){
+            Random random = new Random();
+            int randomLineNumber = random.nextInt(10) + 1;
+            String line = null;
+            for (int i = 1; i <= randomLineNumber; i++) {
+                line = reader.readLine();
+            }
+            return ResponseEntity.status(200).body(line);
+        }catch (IOException e){
+            return ResponseEntity.status(500).body("No such file");
         }
     }
 }
